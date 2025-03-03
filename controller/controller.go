5f5325d765a8f5ba1ae10791b2a1a2b3bafe4b45
Debsnil24 <debsnilsamudra@gmail.com>
2025-03-04 04:15:15 +0530

    Implemented Check Domain Function **Summary** - Successfully Implemented the Check Domain Function - Tested the CLI Application **Testing** - Testing was Successfully with gmail.com and hotmail.com

--- controller/controller.go
+++ controller/controller.go
@@ -1,10 +1,49 @@
 package controller
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func CheckDomain(domain string) {
-	
+	var hasMX, hasSPF, hasDMARC bool
+	var SPFrecord, DMARCrecord string
+
+	MXrecord, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(MXrecord) > 0 {
+		hasMX = true
+	}
+
+	TXTrecord, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range TXTrecord {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			SPFrecord = record
+			break
+		}
+	}
+
+	dmarcRecord, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecord {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			DMARCrecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v\n", domain, hasMX, hasSPF, SPFrecord, hasDMARC, DMARCrecord)
+
 }
